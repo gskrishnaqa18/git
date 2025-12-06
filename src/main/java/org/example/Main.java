8f0ad74c119ee9f60617c8f69641c1806dec34a6
sivah <gskrishnaqa18@gmai.com>
2025-12-06 14:40:19 +0530

    Store Changes

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() throws InterruptedException {
+
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println(String.format("Hello and welcome!"));
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/");
+        Thread.sleep(5000);
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            IO.println("i = " + i);
+        }
+        driver.close();
+    }
+}
---
+++ src/test/java/qa/examples/TestNew.java
@@ -0,0 +1,34 @@
+package qa.examples;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestNew {
+    WebDriver driver;
+    @Test()
+    void test1(){
+        System.out.println("Printing test1 Method");
+
+    }
+    @Test
+    void test2(){
+        System.out.println("Printing Test2 Method");
+    }
+
+    @Test()
+    void test3(){
+        System.out.println("Printing test3 Method");
+    }
+    @Test
+    void test4(){
+        System.out.println("Printing Test4 Method");
+    }
+    @Test(priority = '1')
+    void test5(){
+        System.out.println("Printing test5 Method");
+    }
+}
